75082b6772f906194512775642f55152226a856c
Malivil <mlebson@outlook.com>
2022-01-17 16:30:57 -0500

    More untested elevator logic

--- Create/elevators.lua
+++ Create/elevators.lua
@@ -1,24 +1,82 @@
 os.unloadAPI("buttons")
 os.loadAPI("disk/buttons.lua")
  
-local running = true
 local guiButtons = {}
 local mon = peripheral.find("monitor")
 local mod = peripheral.find("modem")
+
 local myFloor = 1
 local currentFloor = 1
+
+local running = true
 local moving = false
+
+-- Direction control
+local redstone = false
+local redstoneSide = "right"
+
+-- Piston control
+local piston = false
+local pistonSide = "left"
+
+-- Channels
+local movingChan = 100
+local redstoneChan = 101
 
 -- Floor channels
 mod.open(1)
 mod.open(2)
 mod.open(3)
--- Movement channel
-mod.open(100)
+-- Movement reporting channel
+mod.open(movingChan)
+-- Redstone direction channel
+mod.open(redstoneChan)
 
+-- Redstone control
+local handleRedstone = function()
+    rs.setOutput(redstoneSide, redstone)
+end
+local handlePiston = function()
+    rs.setOutput(pistonSide, piston)
+end
+
+-- Messaging
 local sendFloorMessage = function()
     mod.transmit(currentFloor, currentFloor, "1")
 end
+local sendMovingMessage = function(isMoving)
+    mod.transmit(movingChan, movingChan, isMoving and "1" or "0")
+end
+local sendRedstoneMessage = function(isEnabled)
+    -- Don't actually send anything if we're the computer controlling the redstone direction
+    if myFloor == 1 then
+        redstone = isEnabled
+        handleRedstone()
+    else
+        mod.transmit(redstoneChan, redstoneChan, isEnabled and "1" or "0")
+    end
+end
+
+-- Logic
+local handleFloorSwitch = function(oldFloor, pressed)
+    if oldFloor == pressed then return end
+    if oldFloor == myFloor then
+        piston = false
+        handlePiston()
+    end
+    
+    -- Send "up" redstone message
+    if myFloor > oldFloor then
+        sendRedstoneMessage(true)
+    -- Send "down" redstone message
+    else
+        sendRedstoneMessage(false)
+    end
+    
+    -- TODO: Open piston logic (do we need two for the 2nd floor? If not there will be some special logic)
+end
+
+-- UI
 local updateButtons = function()
     buttons.setColor(guiButtons.buttonFirst, colors.white, currentFloor == 1 and colors.green or colors.lightGray)
     buttons.setColor(guiButtons.buttonSecond, colors.white, currentFloor == 2 and colors.green or colors.lightGray)
@@ -27,36 +85,39 @@ local updateButtons = function()
 end
  
 local firstPressed = function()
+    if moving then return end
     if currentFloor == 1 then return end
     print("[ELEVATOR] Player pressed \"First\" button")
     currentFloor = 1
     updateButtons()
     sendFloorMessage()
 end
-local goFirst = function()
-    
+local goFirst = function(oldFloor)
+    handleFloorSwitch(oldFloor, 1)
 end
 
 local secondPressed = function()
+    if moving then return end
     if currentFloor == 2 then return end
     print("[ELEVATOR] Player pressed \"Second\" button")
     currentFloor = 2
     updateButtons()
     sendFloorMessage()
 end
-local goSecond = function()
-    
+local goSecond = function(oldFloor)
+    handleFloorSwitch(oldFloor, 2)
 end
 
 local thirdPressed = function()
+    if moving then return end
     if currentFloor == 3 then return end
     print("[ELEVATOR] Player pressed \"Third\" button")
     currentFloor = 3
     updateButtons()
     sendFloorMessage()
 end
-local goThird = function()
-    
+local goThird = function(oldFloor)
+    handleFloorSwitch(oldFloor, 3)
 end
  
 -- Create the buttons
@@ -74,25 +135,30 @@ while running do
     if eventArray[1] == "modem_message" then
         local channel = eventArray[3]
         local message = eventArray[5]
-        -- Handle floor call methods
+        -- Handle floor call messages
         if channel >= 1 and channel <= 3 then
+            local oldFloor = currentFloor
             currentFloor = channel
             print("[ELEVATOR] Received message from channel " .. channel .. ": " .. message)
             if channel == 1 then
-                goFirst()
+                goFirst(oldFloor)
             elseif channel == 2 then
-                goSecond()
+                goSecond(oldFloor)
             elseif channel == 3 then
-                goThird()
+                goThird(oldFloor)
             end
             updateButtons()
-        end
-
         -- Handle moving message
-        if channel == 100 then
+        else if channel == movingChan then
             moving = message == "1"
             local state = moving and "now" or "no longer"
             print("[ELEVATOR] Elevator is " .. state .. " moving")
+        -- Handle redstone control message
+        else if myFloor == 1 and channel == redstoneChan then
+            redstone = message == "1"
+            local state = redstone and "enabled" or "disabled"
+            print("[ELEVATOR] Redstone is now " .. state)
+            handleRedstone()
         end
     else
         buttons.event(eventArray)
