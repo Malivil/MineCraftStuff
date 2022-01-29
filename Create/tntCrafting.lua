4f0db377f7c72fd9b1c07683cbc9d4d4f72786b7
Malivil <mlebson@outlook.com>
2022-01-29 11:01:45 -0500

    Fixed TNT crafting failing if it had too many items

--- Create/tntCrafting.lua
+++ Create/tntCrafting.lua
@@ -2,33 +2,42 @@ local sandPlace = 1
 local sandCount = 4
 local gunpowderPlace = 2
 local gunpowderCount = 5
-
+local currentCount = 0
+ 
 local function getSupply(count, name, fn)
     -- don't bother getting more if we have enough
     if turtle.getItemCount() >= count then return true end
-
+ 
     if not (fn or turtle.suck)(count) then
         print("Failed to pick up " .. name .. ", please check pathing and shelf access.")
         return false
     end
-
+ 
     if turtle.getItemCount() < count then
         print("Not enough " .. name .. " to continue crafting. Please resupply shelves.")
         return false
     end
     return true
 end
-
+ 
 while (true) do
 -- sand
-	turtle.select(sandPlace)
+    turtle.select(sandPlace)
     while not getSupply(sandCount, "Sand", turtle.suckUp) do
         sleep(30)
+    end
+    currentCount = turtle.getItemCount()
+    if currentCount > sandCount then
+        turtle.dropUp(currentCount - sandCount)
     end
 -- gunpowder
     turtle.select(gunpowderPlace)
     while not getSupply(gunpowderCount, "Gunpowder", turtle.suckDown) do
         sleep(30)
+    end
+    currentCount = turtle.getItemCount()
+    if currentCount > gunpowderCount then
+        turtle.dropDown(currentCount - gunpowderCount)
     end
 -- place sand
     turtle.select(sandPlace)
@@ -47,5 +56,5 @@ while (true) do
     turtle.craft()
     turtle.drop()
 end
-
+ 
 print("Crafting terminated")
